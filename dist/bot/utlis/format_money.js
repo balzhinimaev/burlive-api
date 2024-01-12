ec394bd9bde9626fcf9e0db0ca6d49a8057394f0
Alexey[ZAV] <alexey@operatorts.ru>
2024-01-12 18:48:56 +0800

    update

--- dist/bot/utlis/format_money.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function formatMoney(amount) {
-    return new Intl.NumberFormat('ru-RU').format(amount);
-}
-exports.default = formatMoney;
-//# sourceMappingURL=format_money.js.map
