1d11e604170da36d25f65eca30da6c59485b9f58
alexandr <606620@biletavto.ru>
2025-03-09 21:17:03 +0800

    update

--- api/src/models/Task.ts
+++ api/src/models/Task.ts
@@ -3,9 +3,11 @@ import { Schema, model, Document } from 'mongoose';
 export interface ITask extends Document {
     title: string;
     description?: string;
-    taskType: 'subscription' | 'translation' | 'data_entry' | 'other';
+    taskType: 'subscription' | 'translation' | 'data_entry' | 'friend' | 'other';
     rewardPoints: number;
+    order?: number;
     imageUrl?: string; // необязательное поле для ссылки на изображение
+    telegram_channel?: string;
 }
 
 const TaskSchema: Schema = new Schema(
@@ -14,11 +16,25 @@ const TaskSchema: Schema = new Schema(
         description: { type: String },
         taskType: {
             type: String,
-            enum: ['subscription', 'translation', 'data_entry', 'other'],
+            enum: [
+                'subscription',
+                'translation',
+                'data_entry',
+                'friend',
+                'other',
+            ],
             required: true,
         },
         rewardPoints: { type: Number, required: true, default: 0 },
-        imageUrl: { type: String, default: '' }, // если ссылка не указана, хранится пустая строка
+        order: { type: Number, required: false, default: 0 },
+        imageUrl: {
+            type: String,
+            default: '1741494346318-460603479-photo_2025-03-09_12-24-42.jpg',
+        }, // если ссылка не указана, хранится пустая строка
+        telegram_channel: {
+            type: String,
+            default: 'bur_live',
+        }, // если ссылка не указана, хранится пустая строка
     },
     { timestamps: true },
 );
